2d65027f003c615ef49b6a841cc4be53f52b98e9
AgungPrawoto13 <agungprawoto997@gmail.com>
2025-10-17 08:13:16 +0700

    add query to show stock material

--- .sql
+++ .sql
@@ -17,3 +17,31 @@ from datamart_stok.fact_stocks f
 join datamart_stok.dim_entity d on f.entity_id = d.entity_id
 order by f.qty desc
 
+-- query to show stock material
+create materialized view datamart_stok.mv_stock_summary
+engine = SummingMergeTree()
+order by (material_id)
+as
+select
+	material_id,
+	sum(qty) as total_qty
+from datamart_stok.fact_stocks
+group by 
+	material_id;
+
+INSERT INTO datamart_stok.mv_stock_summary
+SELECT
+    material_id,
+    sum(qty) AS total_qty
+FROM datamart_stok.fact_stocks
+GROUP BY material_id;
+
+
+SELECT 
+m.material_id,
+m.material_name, 
+s.total_qty
+FROM datamart_stok.mv_stock_summary s
+JOIN datamart_stok.dim_material m ON s.material_id = m.material_id
+ORDER BY m.material_id  DESC;
+
